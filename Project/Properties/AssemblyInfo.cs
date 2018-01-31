6347dc50a440419a3899dd956c6fc16af941ed8e
Stéphane Lenclud <github@lenclud.com>
2018-01-31 15:59:35 +0100

    Release v0.1.0 Now publishing Any CPU binary.

--- Project/Properties/AssemblyInfo.cs
+++ Project/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ClassLibrary1")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("SharpLibWin32")]
+[assembly: AssemblyDescription("Expose Win32 APIs for .NET applications")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ClassLibrary1")]
+[assembly: AssemblyCompany("Slions")]
+[assembly: AssemblyProduct("SharpLibWin32")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
