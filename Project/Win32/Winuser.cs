0456f12c3c5c012fc99c2f9c37ca451f230e7ebc
Stéphane Lenclud <github@lenclud.com>
2018-01-31 19:14:41 +0100

    Release v0.1.2 Adding definitions for hook support.

--- Project/Win32/Winuser.cs
+++ Project/Win32/Winuser.cs
@@ -5,6 +5,8 @@ using System.Text;
 
 namespace SharpLib.Win32
 {
+    public delegate int HOOKPROC(int code, IntPtr wParam, IntPtr lParam);
+
     static public partial class Function
     {
         [DllImport("user32", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall, SetLastError = true)]
@@ -18,6 +20,53 @@ namespace SharpLib.Win32
 
         [DllImport("user32", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall, SetLastError = true)]
         public static extern int GetClassName(IntPtr hwnd, StringBuilder lpClassName, int nMaxCount);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall, SetLastError = true)]
+        internal static extern IntPtr SetWindowsHookEx(HookType code, HOOKPROC func, IntPtr hInstance, int threadID);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        internal static extern bool UnhookWindowsHookEx(IntPtr hhook);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall, SetLastError = false)]
+        internal static extern int CallNextHookEx(IntPtr hhook, int code, IntPtr wParam, IntPtr lParam);
+
+    }
+
+    /// Hook Types.
+    public enum HookType : int
+    {
+        WH_MSGFILTER = -1,
+        /// <value>0</value>
+        WH_JOURNALRECORD = 0,
+        /// <value>1</value>
+        WH_JOURNALPLAYBACK = 1,
+        /// <value>2</value>
+        WH_KEYBOARD = 2,
+        /// <value>3</value>
+        WH_GETMESSAGE = 3,
+        /// <value>4</value>
+        WH_CALLWNDPROC = 4,
+        /// <value>5</value>
+        WH_CBT = 5,
+        /// <value>6</value>
+        WH_SYSMSGFILTER = 6,
+        /// <value>7</value>
+        WH_MOUSE = 7,
+        /// <value>8</value>
+        WH_HARDWARE = 8,
+        /// <value>9</value>
+        WH_DEBUG = 9,
+        /// <value>10</value>
+        WH_SHELL = 10,
+        /// <value>11</value>
+        WH_FOREGROUNDIDLE = 11,
+        /// <value>12</value>
+        WH_CALLWNDPROCRET = 12,
+        /// <value>13</value>
+        WH_KEYBOARD_LL = 13,
+        /// <value>14</value>
+        WH_MOUSE_LL = 14
     }
 
 }
