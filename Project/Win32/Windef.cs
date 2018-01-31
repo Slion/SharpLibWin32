81e6b0e812dc0f252f2bf0043e6a0f456d769dca
Stéphane Lenclud <github@lenclud.com>
2018-01-31 18:02:41 +0100

    Release v0.1.1 Added: RECT, GetWindowRect, MoveWindow, GetActiveWindow and GetClassName.

---
+++ Project/Win32/Windef.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharpLib.Win32
+{
+    /// <summary>
+    /// The RECT structure defines a rectangle by the coordinates of its upper-left and lower-right corners.
+    /// </summary>
+    public struct RECT
+    {
+        /// <summary>
+        /// Specifies the x-coordinate of the upper-left corner of the rectangle.
+        /// </summary>
+        public int left;
+
+        /// <summary>
+        /// Specifies the y-coordinate of the upper-left corner of the rectangle.
+        /// </summary>
+        public int top;
+
+        /// <summary>
+        /// Specifies the x-coordinate of the lower-right corner of the rectangle.
+        /// </summary>
+        public int right;
+
+        /// <summary>
+        /// Specifies the y-coordinate of the lower-right corner of the rectangle.
+        /// </summary>
+        public int bottom;
+    }
+
+}
---
+++ Project/Win32/Winuser.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace SharpLib.Win32
+{
+    static public partial class Function
+    {
+        [DllImport("user32", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall, SetLastError = true)]
+        public static extern int GetWindowRect(IntPtr hWnd, ref RECT rect);
+
+        [DllImport("user32", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall, SetLastError = true)]
+        public static extern int MoveWindow(IntPtr hWnd, int x, int y, int w, int h, int repaint);
+
+        [DllImport("user32", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall, SetLastError = false)]
+        public static extern IntPtr GetActiveWindow();
+
+        [DllImport("user32", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall, SetLastError = true)]
+        public static extern int GetClassName(IntPtr hwnd, StringBuilder lpClassName, int nMaxCount);
+    }
+
+}
