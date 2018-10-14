993781cfe55fa835ad07f9981ba487ab7fe2bf29
Stéphane Lenclud <github@lenclud.com>
2018-10-14 11:56:40 +0200

    Release v0.1.6 Adding SendMessage and system commands.

---
+++ Project/Win32/Win32SendMessage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace SharpLib.Win32
+{
+    static public partial class Function
+    {
+        /// Return Type: LRESULT->LONG_PTR->int
+        ///hWnd: HWND->HWND__*
+        ///Msg: UINT->unsigned int
+        ///wParam: WPARAM->UINT_PTR->unsigned int
+        ///lParam: LPARAM->LONG_PTR->int
+        [DllImportAttribute("user32.dll", CharSet = CharSet.Auto)]
+        [return: MarshalAsAttribute(UnmanagedType.SysInt)]
+        public static extern int SendMessage([InAttribute()] IntPtr hWnd, uint Msg, [MarshalAsAttribute(UnmanagedType.SysUInt)] uint wParam, [MarshalAsAttribute(UnmanagedType.SysInt)] int lParam);
+
+
+    }
+}
